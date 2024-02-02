80dd5cb53be4f55de2a8a830e016d697bd8dfc19
Ajita Gupta <ajita.gupta001@gmail.com>
2024-02-02 14:54:40 +0100

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -6,24 +6,31 @@ public class Employee {
 
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter Name:");
-		String name=sc.nextLine();
-		
-		System.out.println("Enter Salary/Day:");
-		int salPerDay=sc.nextInt();
+		String name = sc.nextLine();
 
-		System.out.println("Attendance in Days:");
-		int attendance=sc.nextInt();
-		
-		int salary = salPerDay*attendance;
-		
-		if (salary>=100000)	System.out.print("Hi " + name +", your designation is Team Leader");
-		else if (80000>=salary && salary<100000)	System.out.print("Hi " + name + ", your designation is Senior Developer");
-		else if (60000>=salary && salary<80000)	System.out.print("Hi " + name +", your designation is Junior Developer");
-		else if (40000>=salary && salary<60000)	System.out.print("Hi " + name +", your designation is Trainee");
-		else 	System.out.print("Hi " + name +", your designation is Clerk");
+		System.out.println("Enter Salary/Day:");
+		int salPerDay = sc.nextInt();
+
+		System.out.println("Attendance in Days of that Month:");
+		int attendance = sc.nextInt();
+
+		int salary = salPerDay * attendance;
+		System.out.println(salary);
+
+		if (salary >= 10000) {
+			System.out.print("Hi " + name + ", your designation is Team Leader");
+		} else if (8000 >= salary && salary < 10000) {
+			System.out.print("Hi " + name + ", your designation is Senior Developer");
+		} else if (6000 >= salary && salary < 8000) {
+			System.out.print("Hi " + name + ", your designation is Junior Developer");
+		} else if (4000 >= salary && salary < 6000) {
+			System.out.print("Hi " + name + ", your designation is Trainee");
+		} else {
+			System.out.print("Hi " + name + ", your designation is Clerk");
+		}
 
 	}
 
